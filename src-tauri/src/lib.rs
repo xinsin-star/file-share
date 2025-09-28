5baa0bba66634150a55819339cd64e3c3ca0bcd3
xinsin <2219291455@qq.com>
2025-09-28 11:13:16 +0800

    [feat] init react project

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,16 +3,12 @@
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
-#[tauri::command]
-fn add_num(num: i32) -> i32 {
-    return num * num
-}
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet, add_num])
+        .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,63 +1,32 @@
-import {defineConfig, loadEnv} from "vite";
-import vue from "@vitejs/plugin-vue";
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
-// https://vitejs.dev/config/
-export default defineConfig(async ({mode}) => {
-    const env = loadEnv(mode, process.cwd(), '')
-    return {
-        define: {
-            __APP_ENV__: JSON.stringify(env.APP_ENV),
-        },
-        plugins: [
-            vue(),
-            AutoImport({
-                resolvers: [ElementPlusResolver()],
-            }),
-            Components({
-                resolvers: [ElementPlusResolver()],
-            })
-        ],
-        resolve: {
-            dedupe: [
-                'vue'
-            ],
-            // https://cn.vitejs.dev/config/#resolve-alias
-            alias: {
-                // 设置路径
-                '~': path.resolve(__dirname, './'),
-                // 设置别名
-                '@': path.resolve(__dirname, './src')
-            },
-            // https://cn.vitejs.dev/config/#resolve-extensions
-            extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
-        },
-        // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-        //
-        // 1. prevent vite from obscuring rust errors
-        clearScreen: false,
-        // 2. tauri expects a fixed port, fail if that port is not available
-        server: {
-            port: 1420,
-            strictPort: true,
-            host: host || false,
-            hmr: host
-                ? {
-                    protocol: "ws",
-                    host,
-                    port: 1421
-                }
-                : undefined,
-            watch: {
-                // 3. tell vite to ignore watching `src-tauri`
-                ignored: ["**/src-tauri/**"]
-            }
+// https://vite.dev/config/
+export default defineConfig(async () => ({
+  plugins: [react()],
+
+  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+  //
+  // 1. prevent Vite from obscuring rust errors
+  clearScreen: false,
+  // 2. tauri expects a fixed port, fail if that port is not available
+  server: {
+    port: 1420,
+    strictPort: true,
+    host: host || false,
+    hmr: host
+      ? {
+          protocol: "ws",
+          host,
+          port: 1421,
         }
-    }
-});
+      : undefined,
+    watch: {
+      // 3. tell Vite to ignore watching `src-tauri`
+      ignored: ["**/src-tauri/**"],
+    },
+  },
+}));
